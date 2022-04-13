5c3d78fc0fc1c118f6d6bbcfe06d284e2c97d84b
iiaii <jhun9409@naver.com>
2022-04-13 23:18:22 +0900

    test: TimeDecorator 추가 및 테스트

---
+++ src/test/java/hello/proxy/pureproxy/decorator/code/TimeDecorator.java
@@ -0,0 +1,22 @@
+package hello.proxy.pureproxy.decorator.code;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class TimeDecorator implements Component {
+
+    private final Component component;
+
+    @Override
+    public String operation() {
+        log.info("TimeDecorator 실행");
+        long startTime = System.currentTimeMillis();
+        String result = component.operation();
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeDecorator 종료 reusltTime={}ms", resultTime);
+        return result;
+    }
+}
