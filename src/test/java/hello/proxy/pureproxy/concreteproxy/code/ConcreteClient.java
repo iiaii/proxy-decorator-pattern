911f87ea782d41171e97a7fa9d530c128d79a577
iiaii <jhun9409@naver.com>
2022-04-22 22:37:53 +0900

    test: 구체클래스 기반 프록시 테스트

---
+++ src/test/java/hello/proxy/pureproxy/concreteproxy/code/ConcreteClient.java
@@ -0,0 +1,14 @@
+package hello.proxy.pureproxy.concreteproxy.code;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ConcreteClient {
+
+    private final ConcreteLogic concreteLogic;
+
+    public void execute() {
+        concreteLogic.operation();
+    }
+
+}
