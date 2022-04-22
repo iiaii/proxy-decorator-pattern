4742a301026adb86dcb87151e2643b9c5002f3e6
iiaii <jhun9409@naver.com>
2022-04-22 22:47:05 +0900

    test, feat: 구체 클래스 기반 신규 프록시 추가 및 테스트

--- src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteProxyTest.java
+++ src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteProxyTest.java
@@ -2,6 +2,7 @@ package hello.proxy.pureproxy.concreteproxy;
 
 import hello.proxy.pureproxy.concreteproxy.code.ConcreteClient;
 import hello.proxy.pureproxy.concreteproxy.code.ConcreteLogic;
+import hello.proxy.pureproxy.concreteproxy.code.TimeProxy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +13,15 @@ public class ConcreteProxyTest {
     public void noProxy() {
         ConcreteLogic concreteLogic = new ConcreteLogic();
         ConcreteClient client = new ConcreteClient(concreteLogic);
+        client.execute();
+    }
+
+    @Test
+    @DisplayName("addProxy")
+    public void addProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient client = new ConcreteClient(timeProxy);
         client.execute();
     }
 
