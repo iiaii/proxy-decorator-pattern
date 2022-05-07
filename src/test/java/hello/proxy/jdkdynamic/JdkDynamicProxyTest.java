771a667b8308a5f42a36c4eedbda5298c4138a05
iiaii <jhun9409@naver.com>
2022-05-07 23:22:49 +0900

    test: dynamic proxy 적용 테스트

--- src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
+++ src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
@@ -1,4 +1,39 @@
 package hello.proxy.jdkdynamic;
 
+import hello.proxy.jdkdynamic.code.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Proxy;
+
+@Slf4j
 public class JdkDynamicProxyTest {
+
+    @Test
+    @DisplayName("dynamicA")
+    public void dynamicA() {
+        AImpl target = new AImpl();
+        TimeInvocationHandler handler = new TimeInvocationHandler(target);
+
+        AInterface proxy = (AInterface) Proxy.newProxyInstance(AInterface.class.getClassLoader(), new Class[]{AInterface.class}, handler);
+
+        proxy.call();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+    }
+
+    @Test
+    @DisplayName("dynamicB")
+    public void dynamicB() {
+        BImpl target = new BImpl();
+        TimeInvocationHandler handler = new TimeInvocationHandler(target);
+
+        BInterface proxy = (BInterface) Proxy.newProxyInstance(BInterface.class.getClassLoader(), new Class[]{BInterface.class}, handler);
+
+        proxy.call();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+    }
+
 }
